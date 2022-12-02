02e40b1486392ef3a8a9c5ffad39e7498b3902ef
Sufilevy <sufilevy@gmail.com>
2022-12-02 15:17:27 +0200

    Improved day-1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -2,7 +2,6 @@ use std::{fs, io::Error};
 
 fn main() -> Result<(), Error> {
     let input = fs::read_to_string("input.txt")?;
-    let input = input.split('\n').collect::<Vec<&str>>();
 
     println!("{}", puzzle_one(&input));
     println!("{}", puzzle_two(&input));
@@ -10,7 +9,32 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
-fn puzzle_one(input: &[&str]) -> i32 {
+fn puzzle_one(input: &str) -> i32 {
+    input
+        .split("\n\n")
+        .map(|elf| {
+            elf.split('\n')
+                .map(|cal| cal.parse::<i32>().unwrap())
+                .sum::<i32>()
+        })
+        .max()
+        .unwrap()
+}
+
+fn puzzle_two(input: &str) -> i32 {
+    let mut elves = input
+        .split("\n\n")
+        .map(|elf| {
+            elf.split('\n')
+                .map(|cal| cal.parse::<i32>().unwrap())
+                .sum::<i32>()
+        })
+        .collect::<Vec<_>>();
+    elves.sort_by(|a, b| b.cmp(a));
+    elves.iter().take(3).sum()
+}
+
+fn _puzzle_one(input: &[&str]) -> i32 {
     let mut most_calories = 0;
 
     let mut elf_sum = 0;
@@ -28,13 +52,13 @@ fn puzzle_one(input: &[&str]) -> i32 {
     most_calories
 }
 
-fn puzzle_two(input: &[&str]) -> i32 {
+fn _puzzle_two(input: &[&str]) -> i32 {
     let mut most_calories = [0; 3];
 
     let mut elf_sum = 0;
     for line in input.iter() {
         if line.is_empty() {
-            most_calories = add_elf(most_calories, elf_sum);
+            most_calories = _add_elf(most_calories, elf_sum);
             most_calories.sort();
             most_calories.reverse();
             elf_sum = 0;
@@ -46,7 +70,7 @@ fn puzzle_two(input: &[&str]) -> i32 {
     most_calories.iter().sum()
 }
 
-fn add_elf(most_calories: [i32; 3], elf: i32) -> [i32; 3] {
+fn _add_elf(most_calories: [i32; 3], elf: i32) -> [i32; 3] {
     if elf > most_calories[0] {
         [elf, most_calories[0], most_calories[1]]
     } else if elf > most_calories[1] {
