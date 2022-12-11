b64137940b0e9746d9bda63f5b40e02079f6b22c
Sufilevy <sufilevy@gmail.com>
2022-12-11 15:51:14 +0200

    Finished day-8 part 2

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -10,14 +10,13 @@ fn main() {
 }
 
 fn puzzle_one(input: &[&str]) -> u32 {
-    let (trees, side_len) = parse_trees(input);
-    let mut visible_trees = side_len * 2 + (side_len - 2) * 2;
+    let trees = Trees::parse(input);
+    let mut visible_trees = trees.side_len * 2 + (trees.side_len - 2) * 2; // Border
 
-    for x in 1..side_len - 1 {
-        'y: for y in 1..side_len - 1 {
-            if is_tree_visible(&trees, x, y) {
+    for x in 1..trees.side_len - 1 {
+        for y in 1..trees.side_len - 1 {
+            if trees.is_visible(x, y) {
                 visible_trees += 1;
-                continue 'y;
             }
         }
     }
@@ -25,32 +24,91 @@ fn puzzle_one(input: &[&str]) -> u32 {
     visible_trees as u32
 }
 
-fn parse_trees(input: &[&str]) -> (Array2<u32>, usize) {
-    let side_len = input.len();
-    let mut trees = input
-        .iter()
-        .flat_map(|line| line.chars().map(|c| c.to_digit(10).unwrap()));
-    (
-        Array2::<u32>::from_shape_simple_fn((side_len, side_len), || trees.next().unwrap()),
-        side_len,
-    )
+fn puzzle_two(input: &[&str]) -> u32 {
+    let trees = Trees::parse(input);
+
+    (1..trees.side_len - 1)
+        .map(|x| {
+            (1..trees.side_len - 1)
+                .map(|y| trees.scenic_view_of(x, y))
+                .max()
+                .unwrap()
+        })
+        .max()
+        .unwrap()
 }
 
-fn is_tree_visible(trees: &Array2<u32>, x: usize, y: usize) -> bool {
-    let tree_height = *trees.get((y, x)).unwrap();
+struct Trees {
+    grid: Array2<u32>,
+    side_len: usize,
+}
 
-    let (row, column) = (trees.row(y).to_vec(), trees.column(x).to_vec());
-    let slices = vec![&row[..x], &row[x + 1..], &column[..y], &column[y + 1..]];
+impl Trees {
+    fn parse(input: &[&str]) -> Self {
+        let side_len = input.len();
+        let mut trees = input
+            .iter()
+            .flat_map(|line| line.chars().map(|c| c.to_digit(10).unwrap()));
 
-    for slice in slices {
-        if tree_height > *slice.iter().max().unwrap() {
-            return true;
+        Self {
+            grid: Array2::<u32>::from_shape_simple_fn((side_len, side_len), || {
+                trees.next().unwrap()
+            }),
+            side_len,
         }
     }
 
-    false
-}
+    fn height_of(&self, x: usize, y: usize) -> u32 {
+        *self.grid.get((y, x)).unwrap()
+    }
 
-fn puzzle_two(input: &[&str]) -> u32 {
-    0
+    fn row_column_of(&self, x: usize, y: usize) -> (Vec<u32>, Vec<u32>) {
+        (self.grid.row(y).to_vec(), self.grid.column(x).to_vec())
+    }
+
+    fn is_visible(&self, x: usize, y: usize) -> bool {
+        let tree_height = self.height_of(x, y);
+
+        let (row, column) = self.row_column_of(x, y);
+        let slices = vec![&row[..x], &row[x + 1..], &column[..y], &column[y + 1..]];
+
+        for slice in slices {
+            if tree_height > *slice.iter().max().unwrap() {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn scenic_view_of(&self, x: usize, y: usize) -> u32 {
+        let tree_height = self.height_of(x, y);
+
+        let (row, column) = self.row_column_of(x, y);
+        let (row_left, column_up) = (&mut row[..x].to_vec(), &mut column[..y].to_vec());
+        row_left.reverse();
+        column_up.reverse();
+
+        let slices = vec![
+            &row_left[..],
+            &row[x + 1..],
+            &column_up[..],
+            &column[y + 1..],
+        ];
+
+        let mut scenic_view = 1;
+        for slice in slices {
+            let mut current_scenic_view =
+                slice.iter().take_while(|&tree| *tree < tree_height).count();
+            if current_scenic_view < slice.len() {
+                current_scenic_view += 1;
+            }
+            if current_scenic_view == 0 {
+                return 0;
+            }
+            scenic_view *= current_scenic_view;
+        }
+
+        scenic_view as u32
+    }
 }
