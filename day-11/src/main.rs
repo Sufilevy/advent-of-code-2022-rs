b32b6b2f4975982add2a282a2da4c6f8388b80c8
Sufilevy <sufilevy@gmail.com>
2023-01-04 23:27:18 +0200

    Finished day 11 part 1

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,7 +1,7 @@
 mod monkey;
 
 use monkey::*;
-use std::fs;
+use std::{cmp, fs};
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
@@ -14,12 +14,14 @@ fn puzzle_one(input: &str) -> u32 {
     let mut monkeys: Vec<Monkey> = input.split("\n\r\n").map(Monkey::from_str).collect();
 
     for _ in 0..20 {
-        for monkey in &monkeys {
-            // ...
+        for i in 0..monkeys.len() {
+            while let Some((target_monkey, item)) = monkeys[i].inspect_item() {
+                monkeys[target_monkey].add_item(item);
+            }
         }
     }
 
-    monkeys.sort_by_key(|b| std::cmp::Reverse(b.num_inspections()));
+    monkeys.sort_by_key(|b| cmp::Reverse(b.num_inspections()));
     monkeys[0].num_inspections() * monkeys[1].num_inspections()
 }
 
--- day-11/src/monkey.rs
+++ day-11/src/monkey.rs
@@ -30,6 +30,23 @@ impl Operation {
             Self::Multiply(Target::from_str(target))
         }
     }
+
+    pub fn apply_to(&self, item: &mut u32) {
+        let old = *item;
+
+        let new = match self {
+            Operation::Multiply(target) => match target {
+                Target::Old => old * old,
+                Target::Num(num) => old * num,
+            },
+            Operation::Add(target) => match target {
+                Target::Old => old + old,
+                Target::Num(num) => old + num,
+            },
+        };
+
+        *item = new;
+    }
 }
 
 #[derive(Debug)]
@@ -75,6 +92,30 @@ impl Monkey {
             monkey_b: Self::parse_target(lines[5]),
             num_inspections: 0,
         }
+    }
+
+    /// Returns the target monkey's index and the thrown item's worry level.
+    pub fn inspect_item(&mut self) -> Option<(usize, u32)> {
+        if self.items.is_empty() {
+            return None;
+        }
+        self.num_inspections += 1;
+
+        let mut item = self.items.remove(0);
+
+        self.operation.apply_to(&mut item); // Monkey inspects the item
+
+        item /= 3; // Monkey gets bored
+
+        if item % self.divisible_by == 0 {
+            Some((self.monkey_a as usize, item))
+        } else {
+            Some((self.monkey_b as usize, item))
+        }
+    }
+
+    pub fn add_item(&mut self, item: u32) {
+        self.items.push(item);
     }
 
     pub fn num_inspections(&self) -> u32 {
