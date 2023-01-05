6d60e718cfb7a145479d7f07d59a9b03171c33a3
Sufilevy <sufilevy@gmail.com>
2023-01-06 00:14:00 +0200

    Started day 12

---
+++ day-12/src/climbing.rs
@@ -0,0 +1,142 @@
+use ndarray::Array2;
+
+type Position = (usize, usize);
+type Path = Vec<Position>;
+
+enum PossiblePositions {
+    End(Position),
+    Positions(Vec<Position>),
+}
+
+#[derive(Debug)]
+pub struct HeightMap {
+    map: Array2<u8>,
+    width: usize,
+    height: usize,
+}
+
+impl HeightMap {
+    fn height_from_char(c: u8) -> u8 {
+        match c as char {
+            'S' => 0,
+            'E' => u8::MAX,
+            _ => c - 96,
+        }
+    }
+
+    pub fn from_str(input: &[&str]) -> Self {
+        let (width, height) = (input[0].len(), input.len());
+        let map = Array2::from_shape_fn((height, width), |(y, x)| {
+            Self::height_from_char(input[y].as_bytes()[x])
+        });
+
+        Self { map, width, height }
+    }
+
+    fn get(&self, x: usize, y: usize) -> u8 {
+        self.map[[y, x]]
+    }
+
+    fn get_start(&self) -> Position {
+        for x in 0..self.width {
+            for y in 0..self.height {
+                if self.get(x, y) == 0 {
+                    return (x, y);
+                }
+            }
+        }
+        (0, 0)
+    }
+
+    fn get_possible_positions_for_step(&self, current_position: Position) -> PossiblePositions {
+        let mut positions = Vec::new();
+        let (x, y) = current_position;
+        let current_height = self.get(x, y);
+
+        // Up
+        if y > 0 {
+            let other = self.get(x, y - 1);
+
+            if current_height == 26 && other == u8::MAX {
+                return PossiblePositions::End((x, y - 1));
+            } else if other == current_height || other == current_height + 1 {
+                positions.push((x, y - 1));
+            }
+        }
+
+        // Down
+        if y < self.height - 1 {
+            let other = self.get(x, y + 1);
+
+            if current_height == 26 && other == u8::MAX {
+                return PossiblePositions::End((x, y + 1));
+            } else if other == current_height || other == current_height + 1 {
+                positions.push((x, y + 1));
+            }
+        }
+
+        // Left
+        if x > 0 {
+            let other = self.get(x - 1, y);
+
+            if current_height == 26 && other == u8::MAX {
+                return PossiblePositions::End((x - 1, y));
+            } else if other == current_height || other == current_height + 1 {
+                positions.push((x - 1, y));
+            }
+        }
+
+        // Right
+        if x < self.width - 1 {
+            let other = self.get(x + 1, y);
+
+            if current_height == 26 && other == u8::MAX {
+                return PossiblePositions::End((x + 1, y));
+            } else if other == current_height || other == current_height + 1 {
+                positions.push((x + 1, y));
+            }
+        }
+
+        PossiblePositions::Positions(positions)
+    }
+
+    fn rec_generate_paths(&self, mut path: Path, current_position: Position) -> Vec<Path> {
+        path.push(current_position);
+
+        let positions = self.get_possible_positions_for_step(current_position);
+
+        match positions {
+            PossiblePositions::End(end) => {
+                path.push(end);
+                println!("End");
+                vec![path]
+            }
+            PossiblePositions::Positions(positions) => {
+                let positions: Vec<Position> = positions
+                    .into_iter()
+                    .filter(|position| !path.contains(position))
+                    .collect();
+
+                if positions.is_empty() {
+                    return Vec::new();
+                }
+
+                let mut paths = Vec::new();
+                for position in positions {
+                    paths.extend(self.rec_generate_paths(path.clone(), position).into_iter());
+                }
+
+                paths
+            }
+        }
+    }
+
+    pub fn generate_paths(&self) -> Vec<Path> {
+        let start = self.get_start();
+
+        self.rec_generate_paths(Vec::new(), start)
+            .into_iter()
+            .map(|path| path[1..].to_vec())
+            .collect()
+    }
+}
---
+++ day-12/src/main.rs
@@ -0,0 +1,23 @@
+mod climbing;
+
+use climbing::*;
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let input = input.lines().collect::<Vec<_>>();
+
+    println!("{}", puzzle_one(&input));
+    println!("{}", puzzle_two(&input));
+}
+
+fn puzzle_one(input: &[&str]) -> u32 {
+    let height_map = HeightMap::from_str(input);
+    let mut paths = height_map.generate_paths();
+    paths.sort_by_key(|path| path.len());
+    paths.first().unwrap().len() as u32
+}
+
+fn puzzle_two(_input: &[&str]) -> u32 {
+    0
+}
